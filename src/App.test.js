ab313de0fd23f47241287a5c3d79035981f61bdb
Jakub Žitný <jakubzitny@avocode.com>
2017-02-01 17:37:53 +0100

    add chai and chai-enzyme assertions

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,29 @@
+import _ from 'lodash'
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+
 import App from './App';
+import Big from './Big';
+
+chai.use(chaiEnzyme())
+
 
 it('renders shallowly without crashing', () => {
-  shallow(<App />);
+  const wrapper = shallow(<App />);
+
+  expect(wrapper).to.have.descendants('div');
 });
 
 it('renders without crashing', () => {
-  mount(<App />);
+  const wrapper = mount(<App />);
+
+  expect(wrapper).to.have.descendants('p');
+});
+
+it('renders big component crashing', () => {
+  const wrapper = mount(<Big />);
+
+  expect(wrapper).to.have.descendants('p');
 });
